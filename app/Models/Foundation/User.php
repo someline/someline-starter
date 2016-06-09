ae7dbdb682f7a7fb2ca5e42929f798df21d313b7
Libern <libernlin@gmail.com>
2016-06-09 22:56:40 +0800

    Improved User model

--- app/Models/Foundation/User.php
+++ app/Models/Foundation/User.php
@@ -2,20 +2,46 @@
 
 namespace Someline\Models\Foundation;
 
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Foundation\Auth\Access\Authorizable;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 use Prettus\Repository\Traits\TransformableTrait;
+use Someline\Models\BaseModel;
 
-class User extends Authenticatable implements Transformable
+class User extends BaseModel implements BaseModelEventsInterface,
+    AuthenticatableContract,
+    AuthorizableContract,
+    CanResetPasswordContract,
+    Transformable
 {
+    use Authenticatable, Authorizable, CanResetPassword;
     use TransformableTrait;
-    
+
+    /**
+     * Indicates if the model should be auto set user_id.
+     *
+     * @var bool
+     */
+    protected $autoUserId = false;
+
+    /**
+     * The primary key for the model.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'user_id';
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'slid',
+        'gender', 'birthday', 'country', 'timezone', 'locale', 'username', 'phone_number', 'status',
     ];
 
     /**
@@ -26,4 +52,15 @@ class User extends Authenticatable implements Transformable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * Called when model is created
+     * Other events available are in BaseModelEvents
+     */
+    public function onCreated()
+    {
+        parent::onCreated();
+
+    }
+
 }
