a51618309b728d11c997a2d2e5688fc33d7ec9cb
Libern <libernlin@gmail.com>
2016-06-10 10:52:26 +0800

    Added build scripts to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,35 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+    mix.less('../angulr/css/less/app.less', 'resources/assets/angulr/css/');
+    mix.styles([
+        'public/bower_components/toastr/toastr.css',
+
+        'public/vendors/bower_components/bootstrap/dist/css/bootstrap.css',
+        'public/vendors/bower_components/animate.css/animate.css',
+        'public/vendors/bower_components/font-awesome/css/font-awesome.css',
+        'public/vendors/bower_components/simple-line-icons/css/simple-line-icons.css',
+        'resources/assets/angulr/css/*.css'
+    ], 'public/css/app.src.css', './');
+
+    // scripts
+    // vue js
+    mix.browserify('main.js');
+
+    mix.scripts([
+        'public/vendors/bower_components/jquery/dist/jquery.min.js',
+        'public/vendors/bower_components/bootstrap/dist/js/bootstrap.min.js',
+        'resources/assets/angulr/js/support/ui-*.js',
+
+        // dependencies
+        'public/bower_components/toastr/toastr.js',
+
+        'public/js/main.js',
+    ], 'public/js/app.src.js', './');
+
+    // versions
+    mix.version([
+        "public/css/app.src.css",
+        "public/js/app.src.js",
+    ]);
 });
