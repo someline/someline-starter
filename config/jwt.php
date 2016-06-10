47070e1bd22bac45226b547d23ea687165e19a56
Libern <libernlin@gmail.com>
2016-06-10 10:47:35 +0800

    Set jwt secret key

--- config/jwt.php
+++ config/jwt.php
@@ -21,7 +21,7 @@ return [
     |
     */
 
-    'secret' => env('JWT_SECRET', 'changeme'),
+    'secret' => env('JWT_SECRET', 'SYveqe2OtdMPHJLnOmW6aV7TYA7EWMWg'),
 
     /*
     |--------------------------------------------------------------------------
