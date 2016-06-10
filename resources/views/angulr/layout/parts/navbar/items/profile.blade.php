50035e037e0d7adf69ef6c1fff0331b19e79b58b
Libern <libernlin@gmail.com>
2016-06-10 15:18:03 +0800

    Improved profile

--- resources/views/angulr/layout/parts/navbar/items/profile.blade.php
+++ resources/views/angulr/layout/parts/navbar/items/profile.blade.php
@@ -1,5 +1,6 @@
 <li>
-    <a href="{{url('user/'.auth_user()->user_id)}}">
+    <a href="#">
+    {{--<a href="{{url('user/'.auth_user()->user_id)}}">--}}
         <i class="icon-user fa-fw"></i>
         {{--<span class="badge badge-sm up bg-danger pull-right-xs">2</span>--}}
         {{--<i class="visible-xs-inline-block"></i>--}}
