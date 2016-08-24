a4e46c66a70fdedc9965ba16d084cd9587176ef4
Libern <libernlin@gmail.com>
2016-08-24 23:27:12 +0800

    Improved Users database migration

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Someline\Models\Foundation\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::statement("SET foreign_key_checks=0");
+        DB::table('users')->truncate();
+        DB::statement("SET foreign_key_checks=1");
+
+        User::create([
+//            'slid' => '1',
+            'name' => "Libern Lin",
+            'email' => 'libern@someline.com',
+            'password' => bcrypt('Abc12345'),
+            'remember_token' => str_random(10),
+            'gender' => 'M',
+            'birthday' => '1994-07-08',
+            'country' => 'CN',
+            'timezone' => 'Asia/Shanghai',
+            'locale' => 'en',
+            'username' => 'libern',
+            'phone_number' => '+1234567890',
+            'status' => 1,
+        ]);
+
+        factory(User::class, 50)->create();
+    }
+}
