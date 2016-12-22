03296e39e5b4707f740c51b2816d3f8d4666a03d
Libern <libernlin@gmail.com>
2016-12-22 19:58:44 +0800

    Added web routes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,44 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Auth Routes
+Auth::routes();
+
+// Locale Routes
+Route::group(['prefix' => 'locales'], function () {
+
+    Route::get('/{locale}.js', '\Someline\Support\Controllers\LocaleController@getLocaleJs');
+
+    Route::get('/switch/{locale}', '\Someline\Support\Controllers\LocaleController@getSwitchLocale');
+
+});
+
+// Basic Routes
+Route::get('/home', 'HomeController@index');
+
+// Image Routes
+Route::get('/image/{name}', 'ImageController@showOriginalImage');
+Route::post('/image', 'ImageController@postImage');
+
+// Protected Routes
+Route::group(['middleware' => 'auth'], function () {
+
+    Route::get('/', function () {
+        return redirect('users');
+    });
+
+    Route::get('users', 'UserController@getUserList');
+
+});
+
+// Console Routes
+Route::group(['prefix' => 'console'], function () {
+
+    Route::get('oauth', function () {
+        return view('console.oauth');
+    });
+
+    Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
+
+});
