2e35f618f8f92f8c78037878749fbe187c48bcc6
Libern <libernlin@gmail.com>
2016-12-22 19:41:49 +0800

    Changed api configs

--- config/api.php
+++ config/api.php
@@ -169,7 +169,9 @@ return [
     */
 
     'auth' => [
-
+        'passport' => \Someline\Api\Auth\Provider\Passport::class,
+        'oauth' => \Someline\Api\Auth\Provider\OAuth2::class,
+        'jwt' => Dingo\Api\Auth\Provider\JWT::class,
     ],
 
     /*
