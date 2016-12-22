7fb42e25c8d2472298c9cdcbbe6f549620abad1d
Libern <libernlin@gmail.com>
2016-12-22 23:54:30 +0800

    Improved builds

--- gulpfile.js
+++ gulpfile.js
@@ -16,4 +16,38 @@ require('laravel-elixir-vue-2');
 elixir((mix) => {
     mix.sass('app.scss')
        .webpack('app.js');
+
+    // less
+    mix.less('app.less', 'resources/assets/less/css/');
+
+    // styles
+    mix.styles([
+        // theme style
+        "public/css/theme.src.css",
+
+        // dependencies
+        'public/bower_components/toastr/toastr.css',
+
+        // main styles
+        'resources/assets/less/css/*.css',
+    ], 'public/css/app.src.css', './');
+
+    // scripts
+    mix.scripts([
+        // theme script
+        'public/js/theme.src.js',
+
+        // dependencies
+        'public/bower_components/toastr/toastr.js',
+
+        // main vue js
+        'public/js/app.js',
+    ], 'public/js/app.src.js', './');
+
+    // versions
+    mix.version([
+        "public/css/app.src.css",
+        "public/js/app.src.js",
+    ]);
+
 });
