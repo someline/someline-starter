91045464d0c11b770b653d0bd9c50b07da1c02d3
Libern <libernlin@gmail.com>
2016-12-23 13:00:10 +0800

    Improved mobile layouts

--- resources/views/mobile/layout/parts/mobile_header.blade.php
+++ resources/views/mobile/layout/parts/mobile_header.blade.php
@@ -1,5 +1,5 @@
 <!-- header -->
-<header id="header" class="app-header navbar box-shadow bg-light" role="menu">
+<header id="header" class="app-header navbar box-shadow bg-dark" role="menu">
     @include('mobile.layout.parts.navbar.mobile_nav_header')
 {{--    @include('mobile.layout.parts.navbar.collapse')--}}
     {{--<div ui-butterbar="" class="butterbar hide"><span class="bar"></span></div>--}}
