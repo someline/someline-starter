c60cef47d80d2ece12f1a05fd1cadb65e1c4b616
Libern <libernlin@gmail.com>
2017-01-26 17:13:00 +0800

    Integrating Laravel 5.4

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -28,19 +28,20 @@ require('vue-i18n');
 require('./filters/helpers');
 
 /**
- * We'll register a HTTP interceptor to attach the "CSRF" header to each of
- * the outgoing requests issued by this application. The CSRF middleware
- * included with Laravel will automatically verify the header's value.
+ * We'll load the axios HTTP library which allows us to easily issue requests
+ * to our Laravel back-end. This library automatically handles sending the
+ * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-// Vue.http.headers.common['Accept'] = 'application/x.someline.v1+json';
+window.axios = require('axios');
 
-Vue.http.interceptors.push((request, next) => {
-    request.headers.set('X-CSRF-TOKEN', Laravel.csrfToken);
-    request.headers['Accept-Language'] = Someline.locale;
+window.axios.defaults.headers.common = {
+    'X-Requested-With': 'XMLHttpRequest',
+    // 'Accept': 'application/x.someline.v1+json',
+    'Accept-Language': Someline.locale
+};
 
-    next();
-});
+Vue.prototype.$http = window.axios;
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
