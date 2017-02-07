1e570ec84ed7f7a8ee11f48e69925aebd38c518d
Libern <libernlin@gmail.com>
2017-02-07 18:24:22 +0800

    Improved resources linking

--- resources/views/console/layout/master.blade.php
+++ resources/views/console/layout/master.blade.php
@@ -1,12 +1,12 @@
 @extends('angulr.layout.master')
 
 @push('stylesheets')
-<link rel="stylesheet" href="{{url(mix("assets/css/console.vendor.css"))}}" type="text/css"/>
+<link rel="stylesheet" href="{{url("assets/css/console.vendor.css")}}" type="text/css"/>
 <link rel="stylesheet" href="{{url(mix("assets/css/console.main.css"))}}" type="text/css"/>
 @endpush
 
 @push('scripts')
-<script src="{{url(mix("assets/js/console.vendor.js"))}}"></script>
+<script src="{{url("assets/js/console.vendor.js")}}"></script>
 <script src="{{url(mix("assets/js/manifest.js"))}}"></script>
 <script src="{{url(mix("assets/js/vendor.js"))}}"></script>
 <script src="{{url(mix("assets/js/console.main.js"))}}"></script>
