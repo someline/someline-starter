d38ac243094ed17a70d72f9a62d7b7f27fad25bc
QiuTuLeng <qiutuleng@gmail.com>
2017-10-11 16:14:06 +0800

    fix style

--- config/repository.php
+++ config/repository.php
@@ -226,7 +226,7 @@ return [
         'rootNamespace' => 'Someline\\',
         'paths'         => [
             'models'       => 'Models',
-            'criteria' => 'Repositories\Criteria',
+            'criteria'     => 'Repositories\Criteria',
             'repositories' => 'Repositories\Eloquent',
             'interfaces'   => 'Repositories\Interfaces',
             'transformers' => 'Transformers',
