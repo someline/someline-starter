160a4a655bf7a7a9933156240860e2578e37ebab
Libern <libernlin@gmail.com>
2019-04-08 23:57:22 +0800

    Upgrade to Laravel 5.8.*

--- config/auth.php
+++ config/auth.php
@@ -42,8 +42,9 @@ return [
         ],
 
         'api' => [
-            'driver' => 'passport',
+            'driver' => 'token',
             'provider' => 'users',
+            'hash' => false,
         ],
     ],
 
